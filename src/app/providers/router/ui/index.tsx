0bc99d77be644e53141e736799af97aee1ddbd4c
Maksad Khalipaev <maks14flay@gmail.com>
2025-01-28 20:45:53 +0500

    feat/addScrollTopOnRouteChange

--- src/app/providers/router/ui/index.tsx
+++ src/app/providers/router/ui/index.tsx
@@ -1,14 +1,25 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { PageLoader } from "@app/appLayout/ui/pageLoader";
 import { routeConfig } from "@shared/config/routes";
-import { NavigateFunction, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  NavigateFunction,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 export let routerNavigate: NavigateFunction;
 
 export const AppRouter = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   routerNavigate = navigate;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <Routes>
